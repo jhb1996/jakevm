4d4dc3e49ccb4019f9c8882f21992eab86f510ad
Jake Bareket <bareketjake@yahoo.com>
2018-03-22 11:02:28 -0400

    s

--- pa3/tests.py
+++ pa3/tests.py
@@ -75,9 +75,9 @@ grayImage = cv2.cvtColor(image.astype(np.float32)/255.0, cv2.COLOR_BGR2GRAY)
 def compute_and_save():
     (a,b) = HKD.computeHarrisValues(grayImage) # Todo1
     c = HKD.computeLocalMaxima(a) # Todo2
-    d = HKD.detectKeypoints(image) # Todo3
-    e = SFD.describeFeatures(image, d) # Todo 4
-    f = MFD.describeFeatures(image, d) # Todo 5,6
+    #d = HKD.detectKeypoints(image) # Todo3
+    #e = SFD.describeFeatures(image, d) # Todo 4
+    #f = MFD.describeFeatures(image, d) # Todo 5,6
     # No test for Todo 7 or 8
     d_proc = pickle_cv2(d)
     np.savez('resources/arrays',a=a,b=b,c=c,d_proc=d_proc,e=e,f=f)

0994b1122bba99af8dea8314329006bf742f2b6d
Jake Bareket <bareketjake@yahoo.com>
2018-04-19 10:24:05 -0400

    d

--- pa4/student.py
+++ pa4/student.py
@@ -173,8 +173,8 @@ def project_impl(K, Rt, points):
     print ("shape P =", np.shape(P))
     print ("shape points =", np.shape(points))
     print ("points", points)
-    #shape = np.shape(points)
-    points_homo = np.concatenate(points,1,axis=2)
+    x,y,_ = np.shape(points)
+    points_homo = np.concatenate((points,np.ones((x,y))),axis=2)
     projections = np.dot(P,points_homo) #do I invert P?
     
 
